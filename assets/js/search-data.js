85a8044184088f4cad0436b0b58738cfab958113
Dobid <36502022+Dobid@users.noreply.github.com>
2026-03-26 19:17:08 +0000

    Deploying to gh-pages from @ Dobid/dobid.github.io@d8b99421f83e01c02582bd0eabfbbf837fe9f7b6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -96,7 +96,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/CV_David_Olivares%20(ENG).pdf", "_blank");
         },
       },{
         id: 'social-email',
